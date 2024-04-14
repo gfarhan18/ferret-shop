fc430be2c42213cac747ef5083f9955fb3bb7b2b
Muhammad Farhan <gfarhan18@gmail.com>
2024-04-14 22:44:06 +0500

    more design changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,36 +4,32 @@ import OurMission from "@/components/OurMission/OurMission";
 import Products from "@/components/Products/Products";
 
 const Home = () => {
-    return (
-      <div className="text-center">
-        <Hero />
+  return (
+    <div className="text-center">
+      <Hero />
+          <OurCommunity
+            title="Join Our Community"
+            description="Join a vibrant community of innovators and creators dedicated to making a difference."
+            buttonText="Become a Member"
+            imageUrl="/our-community.jpeg" // Make sure you have an image at public/community-image.jpg
+          />
+      <section className="bg-gray-100">
+      <OurMission
+        title="Our Mission"
+        description="Our mission is to empower every person and every organization on the planet to achieve more."
+        buttonText="Learn More"
+        imageUrl="/our-mission.jpeg" // Make sure you have an image at public/mission-image.jpg
+        />
+        </section>
 
-        <OurMission
-                title="Our Mission"
-                description="Our mission is to empower every person and every organization on the planet to achieve more."
-                buttonText="Learn More"
-                imageUrl="/our-mission.jpeg" // Make sure you have an image at public/mission-image.jpg
-            />
-
-            <div>
-              <section className="bg-gray-100">
-
-            <OurCommunity
-                title="Join Our Community"
-                description="Join a vibrant community of innovators and creators dedicated to making a difference."
-                buttonText="Become a Member"
-                imageUrl="/our-community.jpeg" // Make sure you have an image at public/community-image.jpg
-            />
-              </section>
-        </div>
-        <div className="justify-center w-full block my-4">
-          <h2 className="text-3xl">Our Products</h2>
-          </div>
-
-        <Products />
+        
+      <div className="justify-center w-full block my-4">
+        <h2 className="text-3xl">Our Products</h2>
       </div>
-    );
-  };
-  
-  export default Home;
-  
+
+      <Products />
+    </div>
+  );
+};
+
+export default Home;
